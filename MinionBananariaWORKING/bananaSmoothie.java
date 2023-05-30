99b528679d6f0a685aca987e78564557d8d88be3
vidhitapde <58280638+vidhitapde@users.noreply.github.com>
2023-05-29 19:39:50 -0700

    Add files via upload

---
+++ MinionBananariaWORKING/bananaSmoothie.java
@@ -0,0 +1,25 @@
+import java.awt.*; // access to Container
+import java.awt.event.*; // access to WindowAdapter, WindowEvent
+import javax.swing.*; // access to JFrame and Jcomponents
+import javax.swing.event.*; // access to JSlider events
+import javax.swing.Timer;
+/**
+ * Write a description of class bananaSmoothie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class bananaSmoothie extends food
+{
+    
+    /**
+     * Constructor for objects of class bananaSmoothie
+     */
+    public bananaSmoothie()
+    {
+        super();
+        foodName = "assets/bananaSmoothie.png";
+    }    
+    
+
+}
---
+++ MinionBananariaWORKING/kitchenFrame.java
@@ -0,0 +1,64 @@
+
+import java.awt.*; // access to Container
+import java.awt.event.*; // access to WindowAdapter, WindowEvent
+import javax.swing.*; // access to JFrame and Jcomponents
+import javax.swing.event.*; // access to JSlider events
+import javax.swing.Timer;
+/**
+ * Write a description of class kitchenFrame here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class kitchenFrame extends JFrame implements ActionListener
+{
+    
+    restaurantFrame rest;
+    
+    introFrame fr;
+    restaurant myRestaurant;
+    food currentOrder;
+    bananaSmoothie1 mybs;
+    JButton BSreturnButton;
+    kitchenFrame(introFrame frame){
+        fr= frame;
+        rest = fr.getRest();
+        myRestaurant = rest.getRestaurant();
+        
+        
+        getContentPane().setBackground(new Color(0x87AE73));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(1000,750);
+        setVisible(true);
+        
+        mybs = new bananaSmoothie1(this);
+        
+    }
+    public void actionPerformed(ActionEvent event){
+        if(event.getSource() == BSreturnButton){
+            remove(mybs);
+            this.repaint();
+            setVisible(false);
+            fr.getRest().checkForDone();
+            fr.getRest().setVisible(true);
+        }
+    }
+    public void paintNewOrder(){
+        System.out.println(myRestaurant.getOrderNum());
+        currentOrder = myRestaurant.getMyOrder();
+        System.out.println(currentOrder.getFoodName());
+        if(currentOrder instanceof bananaSmoothie){
+            mybs = new bananaSmoothie1(this);
+            this.add(mybs);
+            System.out.println("hello");
+            BSreturnButton = mybs.getRestaurantButton();
+        }
+        this.repaint();
+        this.revalidate();
+        System.out.println("L");
+        myRestaurant.incrementOrder();
+        
+    }
+    
+}
